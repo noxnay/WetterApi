efd0678f44a99558cbeddbe40705f61a2e63a0c3
noxnay <71770310+noxnay@users.noreply.github.com>
2023-09-08 12:06:20 +0200

    Update js api more data

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ document.addEventListener("DOMContentLoaded", function () {
         `;
 
         weatherInfo.innerHTML = weatherHtml;
+        
+        
     }
 
     function displayError(statusText, errorMessage) {
@@ -56,5 +58,41 @@ document.addEventListener("DOMContentLoaded", function () {
         `;
 
         weatherInfo.innerHTML = errorHtml;
+    }
+    function displayWeather(data) {
+        const cityName = data.name;
+        const temperature = data.main.temp;
+        const weatherDescription = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+        const tempMin = data.main.temp_min;
+        const tempMax = data.main.temp_max;
+        const feelsLike = data.main.feels_like;
+        const pressure = data.main.pressure;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+        const country = data.sys.country;
+        const sunriseTimestamp = data.sys.sunrise * 1000; // Convert to milliseconds
+        const sunsetTimestamp = data.sys.sunset * 1000; // Convert to milliseconds
+    
+        const sunriseTime = new Date(sunriseTimestamp).toLocaleTimeString();
+        const sunsetTime = new Date(sunsetTimestamp).toLocaleTimeString();
+    
+        const weatherHtml = `
+            <h2>Wetter in ${cityName}, ${country}</h2>
+            <p>Temperatur: ${temperature}°C</p>
+            <p>Min. Temperatur: ${tempMin}°C</p>
+            <p>Max. Temperatur: ${tempMax}°C</p>
+            <p>Gefühlte Temperatur: ${feelsLike}°C</p>
+            <p>Beschreibung: ${weatherDescription}</p>
+            <p>Luftdruck: ${pressure} hPa</p>
+            <p>Luftfeuchtigkeit: ${humidity}%</p>
+            <p>Windgeschwindigkeit: ${windSpeed} m/s</p>
+            <p>Sonnenaufgang: ${sunriseTime}</p>
+            <p>Sonnenuntergang: ${sunsetTime}</p>
+            <img src="${iconUrl}" alt="Wetter Icon">
+        `;
+    
+        weatherInfo.innerHTML = weatherHtml;
     }
 });
